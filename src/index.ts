a424d8bee89500b204435345be5081c5a787c57f
leesam <toyboy2@hanmail.net>
2022-08-29 21:18:16 +0900

    fixes category filter for loader

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ export const loadEmojiCategories = (json: string, suppportedPlatforms?: EmojiPla
     const raws: EmojiRaw[] = JSON.parse(json)
     const categorySet: {[key: string] : EmojiCategory} = {}
     const subCategorySet: {[key: string] : EmojiSubCategory} = {}
+    const platforms = suppportedPlatforms
 
-    return raws.reduce((categories, raw) => {
+    const categories = raws.reduce((list, raw) => {
         let category = categorySet[raw.category]
+        
         if (!category) {
             category = { name: raw.category, subCategories: [] }
             categorySet[raw.category] = category
-            categories.push(category)
+            list.push(category)
         }
 
         const subCategoryKey = `${raw.category}_${raw.subcategory}`
@@ -21,18 +23,42 @@ export const loadEmojiCategories = (json: string, suppportedPlatforms?: EmojiPla
             category.subCategories.push(subCategory)
         }
 
+        const isSupportApple = platforms?.apple !== undefined ? raw.has_img_apple === platforms?.apple : true
+        const isSupportGoogle = platforms?.google !== undefined ? raw.has_img_google === platforms?.google : true
+        const isSupportFacebook = platforms?.facebook !== undefined ? raw.has_img_facebook === platforms?.facebook : true
+        const isSupportTwitter = platforms?.twitter !== undefined ? raw.has_img_twitter === platforms?.twitter : true
+
+        const isSupported = isSupportApple && isSupportGoogle &&  isSupportFacebook && isSupportTwitter
+
+        if (!isSupported) return list
+
         subCategory.emojis.push({
             id: raw.sort_order,
             name: raw.name,
             shortNames: raw.short_names,
-            unicodes: raw.unified.split('-').map(code => Number(code)),
-            apple: suppportedPlatforms?.apple ? raw.has_img_apple === suppportedPlatforms?.apple : undefined,
-            google: suppportedPlatforms?.google ? raw.has_img_google === suppportedPlatforms?.google : undefined,
-            facebook: suppportedPlatforms?.facebook ? raw.has_img_facebook === suppportedPlatforms?.facebook : undefined,
-            twitter: suppportedPlatforms?.twitter ? raw.has_img_twitter === suppportedPlatforms?.twitter : undefined
+            unicodes: raw.unified.split('-').map(code => parseInt(code, 16)),
+            ...(platforms &&
+                 { apple: raw.has_img_apple,
+                    google: raw.has_img_google,
+                    facebook: raw.has_img_facebook,
+                    twitter: raw.has_img_twitter
+            })
         })
 
-        return categories
+        return list
+    }, [] as EmojiCategory[])
+
+    return categories.reduce((list, category) => {
+        const filteredSubCategories = category.subCategories.filter(subCategory => subCategory.emojis.length > 0)
+        if (filteredSubCategories.length <= 0) 
+            return list
+
+        list.push({
+            ...category,
+            subCategories: filteredSubCategories     
+        })
+
+        return list
     }, [] as EmojiCategory[])
 }
 
